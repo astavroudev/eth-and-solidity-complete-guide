a16cd845cac687ff4e54a8ac7b2acb04bff2646a
Andreas Stavrou <astavroudev@gmail.com>
2023-02-28 19:50:24 +0200

    create enter lottery function

--- lottery/contracts/Lottery.sol
+++ lottery/contracts/Lottery.sol
@@ -18,4 +18,11 @@ contract Lottery {
         */
         manager = msg.sender;
     }
+
+    //enter lottery function
+    function enter() public payable {
+        require(msg.value > .01 ether);
+
+        players.push(msg.sender);
+    }
 }
