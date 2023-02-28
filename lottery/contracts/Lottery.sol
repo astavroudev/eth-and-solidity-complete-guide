f9c5d80f29803a47c49e4873bd84543f13ecabe5
Andreas Stavrou <astavroudev@gmail.com>
2023-02-28 20:22:28 +0200

    add the random function

--- lottery/contracts/Lottery.sol
+++ lottery/contracts/Lottery.sol
@@ -25,4 +25,8 @@ contract Lottery {
 
         players.push(msg.sender);
     }
+
+        function random() private view returns (uint) {
+        return uint(keccak256(block.difficulty, now, players));
+    }
 }
