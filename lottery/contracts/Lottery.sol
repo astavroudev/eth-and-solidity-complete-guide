1b8fc622e95a2629b8665e7f400e9e8efe5d20ca
Andreas Stavrou <astavroudev@gmail.com>
2023-03-02 20:59:47 +0200

    add require contract owner to call pick winner function

--- lottery/contracts/Lottery.sol
+++ lottery/contracts/Lottery.sol
@@ -31,6 +31,7 @@ contract Lottery {
     }
 
     function pickWinner() public {
+        require(msg.sender == manager);
         uint index = random() % players.length;
         players[index].transfer(this.balance);
         players = new address[](0);
